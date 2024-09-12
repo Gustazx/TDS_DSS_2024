b42ede066b85e86d8c12f8f422d8c73ab3b0ea64
gusta <gustavodelima841@gmail.com>
2024-09-11 21:58:49 -0300

    #AULA-02 - Post e bodyparser

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
 
 const port = 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`);
@@ -14,4 +18,8 @@ app.get("/", (req, res) => {
   } catch (e) {
     res.status(500).send("erro", e);
   }
+});
+
+app.post("/cliente", (req, res) => {
+  res.send("");
 });
