7e6356893c68d5f214ae21d8b1b922a168069d3a
gusta <gustavodelima841@gmail.com>
2024-11-12 22:41:47 -0300

    feat: funcionalidades useState

--- origins/src/App.tsx
+++ origins/src/App.tsx
@@ -1,7 +1,51 @@
 import "./App.css";
+import { useState } from "react";
+import Title from "./Title";
 
 function App() {
-  return <div></div>;
+  const [contador, setContador] = useState(1);
+  const [nome, setNome] = useState("Guilherme");
+  const [email, setEmail] = useState("guilherme.emanuel@sistemafiep.org");
+
+  return (
+    <>
+      <Title />
+      <h2>{contador}</h2>
+      <button
+        onClick={() => {
+          setContador(contador + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setContador(contador - 1);
+        }}
+      >
+        -
+      </button>
+      <h1>Cadastro</h1>
+      {nome} - {email}
+      <div>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => {
+            setNome(e.target.value);
+          }}
+        />
+
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ origins/src/Title.tsx
@@ -0,0 +1,5 @@
+function Title() {
+  return <h2>Contador</h2>;
+}
+
+export default Title;
